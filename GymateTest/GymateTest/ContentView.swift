339d2050674e01586f5f2a37f3396e213c18ee47
Richard Price <mrspondooli@gmail.com>
2019-12-03 17:17:09 +0000

    initial commit

---
+++ GymateTest/GymateTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GymateTest
+//
+//  Created by Richard Price on 03/12/2019.
+//  Copyright © 2019 twisted echo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+//test
