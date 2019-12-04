8cd3eeb677a800963d50d0fd455d1ae6daf53c1f
Richard Price <mrspondooli@gmail.com>
2019-12-04 11:15:41 +0000

    added swift preview for map and setup temp region

--- GymateTest/GymateTest/Main/MainViewController.swift
+++ GymateTest/GymateTest/Main/MainViewController.swift
@@ -10,21 +10,57 @@
 
 import UIKit
 import MapKit
-//MARK:- step 3 import LBTA tools from https://github.com/bhlvoong/LBTATools
 import LBTATools
 
-//MARK:- step 2 create the main controller and import mapkit
 class MainViewController: UIViewController {
+
+    //MARK:- step 4 move map view to gloabal
+    let mapView = MKMapView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let mapView = MKMapView()
         view.addSubview(mapView)
-        
-        //MARK:- Step 4 fill super view to pin map
         mapView.fillSuperview()
-
-        view.backgroundColor = .blue
+        //MARK:- step 5 setup region for map
+        setupRegionForMap()
         
+    }
+    
+    //MARK:- step 4 setup region for map
+    fileprivate func setupRegionForMap() {
+        let centerCoord = CLLocationCoordinate2D(latitude: 51.538368, longitude: -3.205172)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: centerCoord, span: span)
+        mapView.setRegion(region, animated: true)
+
+    }
+    
+}
+
+//MARK:- step 1 import swift UI and create MainPreview with provider
+
+import SwiftUI
+
+struct MainPreview: PreviewProvider {
+
+    static var previews: some View {
+        //MARK:- step 3 add ignore safe area
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    //MARK:- step 2 create another struct Container view, once you have that use make ui view controller
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        func makeUIViewController(context: UIViewControllerRepresentableContext<MainPreview.ContainerView>) -> MainViewController {
+            return MainViewController()
+        }
+        
+        func updateUIViewController(_ uiViewController: MainViewController, context: UIViewControllerRepresentableContext<MainPreview.ContainerView>) {
+            
+            
+        }
+        
+        typealias UIViewControllerType = MainViewController
     }
 }
