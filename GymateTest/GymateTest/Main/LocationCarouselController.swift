41e1b99ab558626038bd0b2c3133121239aca347
Richard Price <mrspondooli@gmail.com>
2019-12-13 15:15:42 +0000

    added didselct select for annotaion selection, refactored into extenstion, added address lable extenions, setup blacknav bar

--- GymateTest/GymateTest/Main/LocationCarouselController.swift
+++ GymateTest/GymateTest/Main/LocationCarouselController.swift
@@ -8,42 +8,78 @@
 
 import UIKit
 import LBTATools
+import MapKit
 
-//MARK: step 2 use LBTA generics to create a horizontal scrolling collection view, first create a location cell
-class LocationCell: LBTAListCell<String> {
+class LocationCell: LBTAListCell<MKMapItem> {
+    
+    //MARK: Step 5 set your didset for when your item cell is passed in
+    override var item: MKMapItem! {
+        didSet {
+            lable.text = item.name
+            addressLable.text = item.address()
+        }
+    }
+    
+    //MARK: Step 3 create a lable and stack it
+    let lable = UILabel(text: "location", font: .boldSystemFont(ofSize: 16))
+    let addressLable = UILabel(text: "address", numberOfLines: 0)
+    
     override func setupViews() {
-        backgroundColor = .yellow
-        //MARK: step 9 setup shadow and corner round
-        setupShadow(opacity: 3, radius: 5, offset: .zero, color: .black)
+        backgroundColor = .white
+        setupShadow(opacity: 3, radius: 5, offset: .zero, color: .orange)
         layer.cornerRadius = 10
         clipsToBounds = false
+        //MARK: Step 3 create a lable and stack it
+        stack(lable, addressLable).withMargins(.allSides(16))
+        
     }
 }
-//MARK: step 3 create annother class for the crousel using the cell
-class LocationsCarouselController: LBTAListController<LocationCell, String>, UICollectionViewDelegateFlowLayout {
+
+class LocationsCarouselController: LBTAListController<LocationCell, MKMapItem > {
     
-    //MARK: step 6 implement delegate flow layout for size of cell with peeking cell
+    //MARK:- step 9 create a reference to the mainController with a weak to prevent retain syscles
+    weak var mainController: MainViewController?
+    
+    //MARK: step 8 add abilty to click on items and cetner to annoation
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(self.items[indexPath.item].name ?? "") // this is our reference for which name to display
+        //MARK:- step 10 centering on annotations
+        let annotations = mainController?.mapView.annotations
+        annotations?.forEach({ (annotation) in
+            if annotation.title == self.items[indexPath.item].name {
+                mainController?.mapView.selectAnnotation(annotation, animated: true)
+            }
+        })
+        
+        //MARK:- step 12 adjust where the carousel displays after clicking on an annotation
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.clipsToBounds = false
+        collectionView.backgroundColor = .clear
+        //MARK: Step 4 create a dummy item cahnge cell types to a mkmpaitem
+        let placeMark = MKPlacemark(coordinate: .init(latitude: 10, longitude: 55))
+        let dummyCell = MKMapItem.init(placemark: placeMark)
+        dummyCell.name = "dummy location"
+        self.items = [dummyCell]
+    }
+}
+
+//MARK:- step 1 create an extension for cleaner code
+extension LocationsCarouselController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return .init(width: view.frame.width - 64, height: view.frame.height)
     }
-    
-    //MARK:- step 7 create an inset
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return .init(top: 0, left: 16, bottom: 0, right: 16)
     }
-    //MARK:- make gap bigger on cells with line spacing
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 24
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //MARK:- step 8 cmake clear bg
-        //MARK: step 9 setup shadow and corner round
-        collectionView.clipsToBounds = false
-        collectionView.backgroundColor = .clear
-        self.items = ["1", "2", "3"]
-    }
 }
-
 
--- GymateTest/GymateTest/Main/MainViewController.swift
+++ GymateTest/GymateTest/Main/MainViewController.swift
@@ -20,35 +20,62 @@ extension MainViewController: MKMapViewDelegate {
 }
 
 class MainViewController: UIViewController {
-
+    
     let mapView = MKMapView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "test"
         mapView.delegate = self
         view.addSubview(mapView)
         mapView.fillSuperview()
         setupRegionForMap()
         performLocalSearch()
-        //MARK: step 5 call function
         setupLocationsCarousel()
+        setupNavBar()
+        //MARK:- step 11 set yor delegate to self, currently unset (or nil) in your locationscontroller
+        locationsController.mainController = self
     }
     
-    //MARK: step 4 call outside view didload
+    //MARK:- step 2 setup a black nav bar with lable, needs refactoring
+    fileprivate func setupNavBar() {
+        let lable: UILabel = {
+            let lable = UILabel(text: "GYMATE", font: UIFont.boldSystemFont(ofSize: 25), textColor: .orange, textAlignment: .left, numberOfLines: 0)
+            return lable
+        }()
+ 
+        let blackView = UIView()
+        blackView.backgroundColor = .black
+
+        let stackView = UIStackView(arrangedSubviews: [blackView])
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.sendSubviewToBack(blackView)
+        
+        let lableStackView = UIStackView(arrangedSubviews: [lable])
+        lableStackView.axis = .horizontal
+        lableStackView.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(stackView)
+        stackView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor)
+        stackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        
+        view.addSubview(lableStackView)
+        lableStackView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 35, left: 150, bottom: 15, right: 15))
+    }
+    
+    
     let locationsController = LocationsCarouselController(scrollDirection: .horizontal)
     
-    //MARK: step 1 create setup locations carosuel function
     fileprivate func setupLocationsCarousel() {
-        //MARK: step 4 call outside view didload
-        let locationsView = locationsController.view!
         
-        //MARK: step 1 (comment out after  generics implemnted)
-//        let locationsView = UIView(backgroundColor: .red)
+        let locationsView = locationsController.view!
+        //        let locationsView = UIView(backgroundColor: .red)
         view.addSubview(locationsView)
-        locationsView.anchor(top: nil, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 16, bottom: 0, right: 16), size: .init(width: 0, height: 150))
+        locationsView.anchor(top: nil, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 16, bottom: 10, right: 16), size: .init(width: 0, height: 150))
     }
-
+    
     fileprivate func performLocalSearch() {
         let request = MKLocalSearch.Request()
         request.naturalLanguageQuery = "Gym"
@@ -63,25 +90,32 @@ class MainViewController: UIViewController {
             if let err = err {
                 print("unable to retrieve search", err)
             }
-
+            //MARK:- step 6 remove old annotations and items
+            self.mapView.removeAnnotations(self.mapView.annotations)
+            self.locationsController.items.removeAll()
+            
             resp?.mapItems.forEach({ (mapItem) in
                 print(mapItem.name ?? "")
                 let annotation = MKPointAnnotation()
                 annotation.coordinate = mapItem.placemark.coordinate
                 annotation.title = mapItem.name
                 self.mapView.addAnnotation(annotation)
+                
+                //MARK:- step 7 tell foreach loop which map items to display
+                self.locationsController.items.append(mapItem)
+                
             })
             self.mapView.showAnnotations(self.mapView.annotations, animated: true)
         }
     }
-
+    
     fileprivate func setupAnnotationsForMap() {
         let annotations = MKPointAnnotation()
         annotations.coordinate = CLLocationCoordinate2D(latitude: 51.535536, longitude: -3.142308)
         annotations.title = "Home"
         annotations.subtitle = "sweet home"
         mapView.addAnnotation(annotations)
-
+        
         let spireHospital = MKPointAnnotation()
         spireHospital.coordinate = .init(latitude: 51.531330, longitude: -3.141665)
         spireHospital.title = "Spire Hospital"
@@ -89,7 +123,7 @@ class MainViewController: UIViewController {
         mapView.showAnnotations(self.mapView.annotations, animated: true)
         
     }
-
+    
     fileprivate func setupRegionForMap() {
         let centerCoord = CLLocationCoordinate2D(latitude: 51.535536, longitude: -3.142308)
         let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
@@ -102,12 +136,12 @@ class MainViewController: UIViewController {
 import SwiftUI
 
 struct MainPreview: PreviewProvider {
-
-    static var previews: some View {
     
+    static var previews: some View {
+        
         ContainerView().edgesIgnoringSafeArea(.all)
     }
-
+    
     struct ContainerView: UIViewControllerRepresentable {
         func makeUIViewController(context: UIViewControllerRepresentableContext<MainPreview.ContainerView>) -> MainViewController {
             return MainViewController()
